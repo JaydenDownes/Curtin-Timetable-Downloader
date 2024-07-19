ba87d8c5c614eb2cb40c3e6e5227fb264391710e
Jayden Downes <35357527+JaydenDownes@users.noreply.github.com>
2024-07-19 19:17:47 +0800

    Basic layouts & Functionality

---
+++ static/tailwind.config.js
@@ -0,0 +1,66 @@
+module.exports = {
+  content: ["./*.html", "./assets/**/*.js"],
+  darkMode: 'selector',
+  theme: {
+    screens: {
+      sm: "540px",
+      // => @media (min-width: 576px) { ... }
+
+      md: "720px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "960px",
+      // => @media (min-width: 992px) { ... }
+
+      xl: "1140px",
+      // => @media (min-width: 1200px) { ... }
+
+      "2xl": "1320px",
+      // => @media (min-width: 1400px) { ... }
+    },
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        black: "#313131",
+        "dark-700": "#27272a",
+        dark: {
+          DEFAULT: "#18181b",
+          2: "#27272a",
+          3: "#3f3f46",
+          4: "#52525b",
+          5: "#71717a",
+          6: "#a1a1aa",
+          7: "#d4d4d8",
+          8: "#e4e4e7",
+        },
+        primary: "#3758F9",
+        "blue-dark": "#444444",
+        secondary: "#13C296",
+        "body-color": "#637381",
+        "body-secondary": "#8899A8",
+        warning: "#FBBF24",
+        stroke: "#DFE4EA",
+        "gray-1": "#F9FAFB",
+        "gray-2": "#F3F4F6",
+        "gray-7": "#CED4DA",
+      },
+      boxShadow: {
+        input: "0px 7px 20px rgba(0, 0, 0, 0.03)",
+        form: "0px 1px 55px -11px rgba(0, 0, 0, 0.01)",
+        pricing: "0px 0px 40px 0px rgba(0, 0, 0, 0.08)",
+        "switch-1": "0px 0px 5px rgba(0, 0, 0, 0.15)",
+        testimonial: "0px 10px 20px 0px rgba(92, 115, 160, 0.07)",
+        "testimonial-btn": "0px 8px 15px 0px rgba(72, 72, 138, 0.08)",
+        1: "0px 1px 3px 0px rgba(166, 175, 195, 0.40)",
+        2: "0px 5px 12px 0px rgba(0, 0, 0, 0.10)",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
